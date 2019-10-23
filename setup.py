8170290fda5a8decbf0a373edd2ce0a9a48481c6
Thorsten Hapke <thorsten.hapke@sap.com>
2019-10-23 16:06:52 +0200

    update

--- setup.py
+++ setup.py
@@ -4,16 +4,17 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="textFieldParser",
-    version="0.0.1",
+    name="textfieldParser",
+    version="0.0.2",
     author="Thorsten Hapke",
     author_email="thorsten.hapke@sap.com",
     description="Parsing Textfields",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/thhapke/textFieldParser",
+    url="https://github.com/thhapke/textfieldParser",
     keywords = ['textfield parsing'],
     packages=setuptools.find_packages(),
+    install_requires=[],
     classifiers=[
     	'Programming Language :: Python :: 3.5',
     	'Programming Language :: Python :: 3.6',
